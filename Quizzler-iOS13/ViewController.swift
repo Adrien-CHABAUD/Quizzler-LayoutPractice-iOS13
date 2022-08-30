2cdf2bea49d323475d6cf7be702a0d8b7239df65
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-08-29 22:23:42 -0400

    Make code more dynamic.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is greater than One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionLabel.text = quiz[questionNumber]
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
